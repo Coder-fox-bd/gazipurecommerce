b849676de33935a9a1762d807a518413c66a004d
Akram Hossain <35065634+Coder-fox-bd@users.noreply.github.com>
2021-07-21 21:18:22 +0600

    sociolite link changes

--- config/services.php
+++ config/services.php
@@ -33,13 +33,13 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/login/google/callback',
+        'redirect' => 'https://gazipurecommerce.com/login/google/callback',
     ],
 
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/login/facebook/callback',
+        'redirect' => 'https://gazipurecommerce.com/login/facebook/callback',
     ],
 
 ];
