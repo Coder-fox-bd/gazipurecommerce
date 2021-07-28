4c27a6b742e474029397e8e177b1f34786d9f94c
Akram Hossain <35065634+Coder-fox-bd@users.noreply.github.com>
2021-07-28 15:51:43 +0600

    admin product datatable responsive

--- resources/views/admin/products/list.blade.php
+++ resources/views/admin/products/list.blade.php
@@ -20,20 +20,9 @@
             <div class="col-md-12">
                 <div class="tile">
                     <div class="tile-body">
-                        <table id="example" class="table table-hover table-bordered" id="dataTable" width="100%" cellspacing="0">
-                            <thead>
-                            <tr>
-                                <th> # </th>
-                                <th> SKU </th>
-                                <th> Name </th>
-                                <th class="text-center"> Brand </th>
-                                <th class="text-center"> Categories </th>
-                                <th class="text-center"> Price </th>
-                                <th class="text-center"> Status </th>
-                                <th style="width:100px; min-width:100px;" class="text-center text-danger"><i class="fa fa-bolt"> </i></th>
-                            </tr>
-                            </thead>
-                            <tfoot>
+                        <div class="table-responsive">
+                            <table class="table table-hover table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <thead>
                                 <tr>
                                     <th> # </th>
                                     <th> SKU </th>
@@ -44,37 +33,50 @@
                                     <th class="text-center"> Status </th>
                                     <th style="width:100px; min-width:100px;" class="text-center text-danger"><i class="fa fa-bolt"> </i></th>
                                 </tr>
-                            </tfoot>
-                            <tbody>
-                                @foreach($products as $product)
+                                </thead>
+                                <tfoot>
                                     <tr>
-                                        <td>{{ $product->id }}</td>
-                                        <td>{{ $product->sku }}</td>
-                                        <td>{{ $product->name }}</td>
-                                        <td>{{ $product->brand->name }}</td>
-                                        <td>
-                                            @foreach($product->categories as $category)
-                                                <span class="badge badge-info">{{ $category->name }}</span>
-                                            @endforeach
-                                        </td>
-                                        <td>{{ config('settings.currency_symbol') }}{{ $product->price }}</td>
-                                        <td class="text-center">
-                                            @if ($product->status == 1)
-                                                <span class="badge badge-success">Active</span>
-                                            @else
-                                                <span class="badge badge-danger">Not Active</span>
-                                            @endif
-                                        </td>
-                                        <td class="text-center">
-                                            <div class="btn-group" role="group" aria-label="Second group">
-                                                <a href="{{ route('admin.product.edit', $product->id) }}" class="btn btn-sm btn-primary"><i class="fa fa-edit"></i></a>
-                                                <a href="{{ route('admin.product.edit', $product->id) }}" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></a>
-                                            </div>
-                                        </td>
+                                        <th> # </th>
+                                        <th> SKU </th>
+                                        <th> Name </th>
+                                        <th class="text-center"> Brand </th>
+                                        <th class="text-center"> Categories </th>
+                                        <th class="text-center"> Price </th>
+                                        <th class="text-center"> Status </th>
+                                        <th style="width:100px; min-width:100px;" class="text-center text-danger"><i class="fa fa-bolt"> </i></th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                </tfoot>
+                                <tbody>
+                                    @foreach($products as $product)
+                                        <tr>
+                                            <td>{{ $product->id }}</td>
+                                            <td>{{ $product->sku }}</td>
+                                            <td>{{ $product->name }}</td>
+                                            <td>{{ $product->brand->name }}</td>
+                                            <td>
+                                                @foreach($product->categories as $category)
+                                                    <span class="badge badge-info">{{ $category->name }}</span>
+                                                @endforeach
+                                            </td>
+                                            <td>{{ config('settings.currency_symbol') }}{{ $product->price }}</td>
+                                            <td class="text-center">
+                                                @if ($product->status == 1)
+                                                    <span class="badge badge-success">Active</span>
+                                                @else
+                                                    <span class="badge badge-danger">Not Active</span>
+                                                @endif
+                                            </td>
+                                            <td class="text-center">
+                                                <div class="btn-group" role="group" aria-label="Second group">
+                                                    <a href="{{ route('admin.product.edit', $product->id) }}" class="btn btn-sm btn-primary"><i class="fa fa-edit"></i></a>
+                                                    <a href="{{ route('admin.product.edit', $product->id) }}" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
