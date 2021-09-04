cfc03a7c198ee61170f3a6389914eeb06bcb373f
Coder-fox-bd <ahrabbi121@gmail.com>
2021-09-04 23:42:44 +0600

    filter for mobile updated

--- resources/views/livewire/user/partials/filter.blade.php
+++ resources/views/livewire/user/partials/filter.blade.php
@@ -1,6 +1,19 @@
+<div class="container">
+    <style>
+        @media (min-width: 768px) {
+            .collapse.dont-collapse-sm {
+                display: block;
+                height: auto !important;
+                visibility: visible;
+            }
+        }
+    </style>
+    <a class="d-xl-none d-lg-none d-md-none text-secondary float-right h4 pr-2" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        <i class="fas fa-sliders-h"></i>
+    </a>
+</div>
 <aside class="col-md-3">
-                    
-    <div class="right-border">
+    <div class="right-border collapse dont-collapse-sm" id="collapseExample">
         <article class="filter-group">
             <header class="card-header">
                 <a href="#" data-toggle="collapse" data-target="#collapse_1" aria-expanded="true">
